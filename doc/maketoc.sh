f664de837432cc743381b891d01b82ff43aa5801
Norihiro Kumagai <tendai22plus@gmail.com>
2023-07-10 19:03:14 +0900

    Refining maketoc.sh, and add {tag} phrases in "##","###",...

--- doc/maketoc.sh
+++ doc/maketoc.sh
@@ -7,12 +7,14 @@ grep '^#' /dev/null  `sed -n '/entry:  *\[/,/^[    ]*\]/{
     s/", *//
     p
 }'` |
-sed 's/:###### / 6 /
+sed '/{[^{}}]*}$/!d
+    s/:###### / 6 /
     s/:##### / 5 /
     s/:#### / 4 /
     s/:### / 3 /
     s/:## / 2 /
-    s/:# / 1 /' |
+    s/:# / 1 /
+    s/^\([^ ]*\) \([^{}]*\){\([^{}]*\)}/\1#\3 \2/' |#cat; exit;
 awk '#
 BEGIN {
     print "## 目次"
@@ -27,10 +29,6 @@ prev < $2 {   # ダイブイン
             printf "  "
         }
         print "<ul>"
-        #for (j = 0; j < i; ++j) {
-        #    printf "  "
-        #}
-        #printf "<li>"
     }
     if ($2 == 3) { section=0; }
     if ($2 == 4) { subsection=0; }
@@ -65,6 +63,7 @@ END {
     if ($2 == 2) { tag="toc-chapter"; chapter++; n=chapter; }
     if ($2 == 3) { tag="toc-section"; section++; n=section; }
     if ($2 == 4) { tag="toc-subsection"; subsection++; n=subsection; }
+    if ($2 == 5) { tag="toc-column"; column++; n=column; }
     if (prev == $2) {
          for (i = 0; i < prev; ++i) {
             printf "  "
@@ -74,8 +73,8 @@ END {
     for (i = 0; i < $2; ++i) {
         printf "  "
     }
-    printf "<li><a href=\"%s#%d-%d\" class=\"%s\">#%s</a>", $1, $2, n, tag, $0;
+    printf "<li><a href=\"%s\" class=\"%s\">#%s</a>", $1, tag, $0;
 }
 { prev=$2; }' |
-sed 's/>#[^# ]* [^ ]* />/'
+sed 's/>#[^ ]* [^ ]* />/'
 
